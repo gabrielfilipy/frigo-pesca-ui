d2dde2e8a5a92dc899139410839b3055ab26ac94
gabrielfilipy <gabrielfilipycorrea@gmail.com>
2025-01-24 21:02:33 -0300

    Firts commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const mobileMenu = document.getElementById('mobileMenu');
+    if (mobileMenu.style.display === 'block') {
+         mobileMenu.style.display = 'none';
+    } else {
+        mobileMenu.style.display = 'block';
+    }
+}
