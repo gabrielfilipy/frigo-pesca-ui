dc7569e7ffcafc2e691fc248880ddbbb359bf43c
gabrielfilipy <gabrielfilipycorrea@gmail.com>
2025-03-07 10:22:42 -0300

    Add menu.

--- js/script.js
+++ js/script.js
@@ -6,3 +6,17 @@ function toggleMenu() {
         mobileMenu.style.display = 'block';
     }
 }
+
+document.querySelectorAll('.mobile-menu a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href'); 
+        const targetElement = document.querySelector(targetId);
+        targetElement.scrollIntoView({
+            behavior: 'smooth'
+        });
+
+        document.getElementById('mobileMenu').style.display = 'none';
+    });
+});
