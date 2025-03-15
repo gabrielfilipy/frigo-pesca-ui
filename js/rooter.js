d19b5a551b46dbe618662c5a564acfbeba161ce0
gabrielfilipy <gabrielfilipycorrea@gmail.com>
2025-03-15 18:23:37 -0400

    Melhorando estrutura do projeto.

---
+++ js/rooter.js
@@ -0,0 +1,14 @@
+
+function home() {
+    window.location.href = "index.html";
+}
+
+
+function products() {
+    window.location.href = "products.html";
+}
+
+
+function contact() {
+    window.location.href = "contact.html";
+}
